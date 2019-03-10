a9c871a35312b875ef640321c6151241a20ecc8e
XEonAX <sv.xeon@gmail.com>
2019-03-10 20:02:39 +0530

    update release script

--- release.bat
+++ release.bat
@@ -2,8 +2,9 @@ for /f "delims== tokens=1,2" %%G in (VERSION) do set anx%%G=%%H
 echo %anxversion%
 git tag %anxversioncode%.master
 git push origin %anxversioncode%.master
-rem ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraMagisk.zip -m master.BleedingEdge BleedingEdge.master
-rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXCameraMagisk.zip -m %anxversion% %anxversioncode%
-..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXScreenRecorder_%anxversion%.apk -m master.%anxversion% %anxversioncode%.master
-REM ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXCameraUnity_%anxversion%.zip -m master.%anxversion% %anxversioncode%.master
+rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXAppsMagisk.zip -m master.BleedingEdge BleedingEdge.master
+rem ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXAppsMagisk.zip -m %anxversion% %anxversioncode%
+REM ..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXScreenRecorder_%anxversion%.apk -m master.%anxversion% %anxversioncode%.master
+..\ANXMiuiPortTools\hub.exe release create -a .\out\ANXAppsMagisk_%anxversion%.zip -m master.%anxversion% %anxversioncode%.master
+..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANXAppsUnity_%anxversion%.zip -m master.%anxversion% %anxversioncode%.master
 REM ..\ANXMiuiPortTools\hub.exe release edit -a .\out\ANX4K60Unity_%anxversion%.zip -m master.%anxversion% %anxversioncode%.master
